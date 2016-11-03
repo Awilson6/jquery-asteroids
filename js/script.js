8947d3ea98db4edf6f9a6238cb3f6073aa8a05e4
Awilson6 <dre_faith@yahoo.com>
2016-11-03 10:43:23 -0600

    We added a variable and function
    We added a variable asteroid and function the creates a column
    containing the asteroilds name

--- js/script.js
+++ js/script.js
@@ -6,5 +6,18 @@ $.ajax({
     $ ("#count").html(results.element_count)
     //Below is another way to, but loner and ungainly.
     //document.getElementsByClassName("count").innerHTML = results.element_count;
+
+    var asteroids = results.near_earth_objects["2015-09-08"];
+    var asteroid;
+    for(var i = 0; i < asteroids.length; i ++) {
+
+      asteroid = $("<h3></h3>").html(asteroids[i].name);
+      $(".container").append(asteroid);
     }
+  }
 });
+//funciton creates a colunm containing the asteroids name.
+function makeName(asteroid){
+
+
+}
