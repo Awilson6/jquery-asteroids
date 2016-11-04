0080fabbfb37c3d6950f70581d2db88f478a9c86
Awilson6 <dre_faith@yahoo.com>
2016-11-04 11:13:08 -0600

    We are creating a makesize fucntion

--- js/script.js
+++ js/script.js
@@ -12,17 +12,20 @@ $.ajax({
     var currentRow;
     for(var i = 0; i < asteroids.length; i ++) {
 
-      if (i % 3 === 0) {
+       {
          currentRow = $("<div></div>").addClass("row");
          $(".container").append(currentRow);
       }
       makeName(asteroids[i], currentRow);
+      makeSize(asteroids[i], currentRow);
+      makeClose(asteroids[i], currentRow);
 
       // asteroid = $("<h3></h3>").html(asteroids[i].name);
       // $(".container").append(asteroid);
     }
   }
 });
+
 //funciton creates a colunm containing the asteroids name.
 function makeName(asteroid, row){
   var colunm = $("<div></div>").addClass("col-md-4");
@@ -31,4 +34,24 @@ function makeName(asteroid, row){
   $(colunm).append(name);
   $(row).append(colunm);
 
+}
+function makeSize(asteroid, row){
+  var colunm = $ ("<div></div>").addClass("col-md-4");
+
+  // TODO: I should calculate the average size of the asteriod, rather then just
+  // use it's max diameter.
+  var size = $ ("<h4></h4>").html(asteroid.estimated_diameter.feet.estimated_diameter_max);
+
+  $(colunm).append(size);
+  $(row).append(colunm);
+}
+
+// This fucntion is used to create the third colunm, which contains close
+// approch data about this asteroid.
+function makeClose(asteroid, row){
+  var colunm = $("<div></div>").addClass("col-md-4");
+  var speed = $("<h4></h4>").html(asteroid.close_approach_data[0].relative_velocity.miles_per_hour);
+
+  $(colunm).append(speed);
+  $(row).append(colunm);
 }
